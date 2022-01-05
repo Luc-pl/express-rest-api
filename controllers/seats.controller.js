bd787715c30522c1ee3e6f86469ff1af9ae252e6
Łukasz Mijała <lukasz.mijala@gmail.com>
2022-01-06 00:13:31 +0100

    Added schema to collections

---
+++ controllers/seats.controller.js
@@ -0,0 +1,75 @@
+const Seats = require('../models/seats.model');
+
+exports.getAll = async (req, res) => {
+  try {
+    res.json(await Seats.find().populate('concert'));
+  }
+  catch (err) {
+    res.status(500).json({message: err});
+  }
+};
+
+exports.getById = async (req, res) => {
+  try {
+    const s = await Seats.findById(req.params.id).populate('concert');
+    if (!s) res.status(404).json({ message: 'Not found' });
+    else res.json(s);
+  }
+  catch (err) {
+    res.status(500).json({message: err});
+  }
+};
+
+exports.post = async (req, res) => {
+  const { seat, client, email, concert } = req.body;
+  try {
+    await Concert.exists({_id: concert});
+    const isTaken = await Seats.exists({concert: concert, seat: seat});
+    if (isTaken) {
+      res.status(409).json({ message: 'The seat is already taken...'});
+    } else {
+        const newSeat = new Seats({ seat, client, email, concert});
+        await newSeat.save();
+        res.json({ message: 'OK' });   
+      }
+  }
+  catch (err) {
+    res.status(500).json({message: err});
+  }
+};
+
+exports.put = async (req, res) => {
+  const { seat, client, email, concert } = req.body;
+  try {
+      await Concert.exists({_id: concert});
+      const isTaken = await Seats.exists({concert: concert, seat: seat});
+      if (isTaken) {
+        res.status(409).json({ message: 'The seat is already taken...'});
+      } else {
+          const s = await (Seats.findById(req.params.id));
+          if (s) {
+            Object.assign(s, {seat, client,email, concert});
+            const newSeat = await s.save();
+            res.json(newSeat);  
+          }
+        else res.status(404).json({ message: 'Not found...' });
+      }
+  }
+  catch (err) {
+    res.status(500).json({message: err});
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    const s = await (Seats.findById(req.params.id));
+    if (s) {
+      await s.remove();
+      res.json(s);
+    }
+    else res.status(404).json({ message: 'Not found...' });
+  }
+  catch (err) {
+    res.status(500).json({message: err});
+  }
+}; 
